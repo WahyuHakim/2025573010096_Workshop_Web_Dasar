e8b8385c6cc43ee90f17fdac49084b7ecd3af6fc
Wahyu Hakim <wahyuhakim2828@gmail.com>
2026-04-28 09:06:47 +0700

    bagian-4-komponen UI & dark mode

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,16 +1,15 @@
-// main.js
-import './style.css'
+import "./style.css";
 // Dark mode toggle
-const btn = document.getElementById('toggleDark')
-const html = document.documentElement
+const btn = document.getElementById("toggleDark");
+const html = document.documentElement;
 // Cek preferensi tersimpan
-if (localStorage.getItem('theme') === 'dark') {
-html.classList.add('dark')
-btn.textContent = 'light'
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  btn.textContent = "light";
 }
-btn.addEventListener('click', () => {
-    html.classList.toggle('dark')
-const isDark = html.classList.contains('dark')
-btn.textContent = isDark ? 'light' : 'dark'
-localStorage.setItem('theme', isDark ? 'dark' : 'light')
-})
+btn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  const isDark = html.classList.contains("dark");
+  btn.textContent = isDark ? "light" : "dark";
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
